3c0eeb5014b5130ff4a0ca8dd872f1a79390327a
emilyprivett <78056325+emilyprivett@users.noreply.github.com>
2022-11-16 13:56:04 -0600

    Update index.php

--- index.php
+++ index.php
@@ -7,20 +7,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
   </head>
   <body>
-<?php
-$servername = "localhost";
-$username = "emilypri_skeco";
-$password = "Projectdbskeco";
-$dbname = "emilypri_skeco";
- 
-// Create connection 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-?>
     <h1 style="text-align:center;">Final Project</h1>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
