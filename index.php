631af049226741b0f4a8578df864298a2071bb34
emilyprivett <78056325+emilyprivett@users.noreply.github.com>
2022-11-28 14:10:05 -0600

    Update index.php

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
     <title>Final Project</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
   </head>
-  <body>
-     <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #301781">
-        <a class="navbar-brand" href="#">Barske</a>
+  <body style="background-color: #301781">
+     <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #91DFF1">
+        <a class="navbar-brand" href="index.html">Barske</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
