7311c332b5212eb855cc57ed60acbeda21e11a04
emilyprivett <78056325+emilyprivett@users.noreply.github.com>
2022-11-28 14:07:46 -0600

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,28 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
   </head>
   <body>
+     <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #301781">
+        <a class="navbar-brand" href="#">Barske</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="beehive.html">The Beehive</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="orchard.html">The Orchard</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="tiki.html">The Tipsy Tiki</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="review.html">Drink Reviews</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
     <h1 style="text-align:center;">Final Project</h1>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
