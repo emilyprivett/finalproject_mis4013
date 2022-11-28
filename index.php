61e63bb9ac86ec3934025b08411d5931aa2a8702
sydneycherek <78374649+sydneycherek@users.noreply.github.com>
2022-11-28 14:39:59 -0600

    Update index.php

--- index.php
+++ index.php
@@ -35,7 +35,23 @@
   <div class="card-body">
     <h5 class="card-title">The Beehive</h5>
     <p class="card-text">564 Moore Avenue, Dallas TX, 75207</p>
-    <a href="beehive.html" class="btn btn-primary">The Beehive</a>
+    <a href="beehive.php" class="btn btn-primary">The Beehive</a>
+  </div>
+      
+      <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="orchard.png">
+  <div class="card-body">
+    <h5 class="card-title">The Orchard</h5>
+    <p class="card-text">2621 Brooke Street, Houston TX, 77035</p>
+    <a href="orchard.php" class="btn btn-primary">The Orchard</a>
+  </div>
+        
+        <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="tiki.png">
+  <div class="card-body">
+    <h5 class="card-title">The Tipsy Tiki</h5>
+    <p class="card-text">934 Short Street, Austin TX, 78723</p>
+    <a href="tiki.php" class="btn btn-primary">The Tipsy Tiki</a>
   </div>
 </div>
     
