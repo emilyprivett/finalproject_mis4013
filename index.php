ea131b74970140d687d0c906de06c7bbc5eb4fed
emilyprivett <78056325+emilyprivett@users.noreply.github.com>
2022-11-28 14:14:27 -0600

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,20 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
   </head>
   <body style="background-color: #F8EDDB">
+    <?php
+$servername = "localhost";
+$username = "emilypri_skeco";
+$password = "Projectdbskeco";
+$dbname = "emilypri_skeco";
+ 
+// Create connection 
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+?>
      <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #A6B6E9">
         <a class="navbar-brand" href="index.html">Barske</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
