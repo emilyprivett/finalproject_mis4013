a5980d2eca5d2ec13d99073a00e60677c354fb65
emilyprivett <78056325+emilyprivett@users.noreply.github.com>
2022-11-28 14:22:42 -0600

    Update index.php

--- index.php
+++ index.php
@@ -7,22 +7,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
   </head>
   <body style="background-color: #F8EDDB">
-    <?php
-$servername = "localhost";
-$username = "emilypri_skeco";
-$password = "Projectdbskeco";
-$dbname = "emilypri_skeco";
- 
-// Create connection 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-?>
      <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #A6B6E9">
-        <a class="navbar-brand" href="index.html"><img src="barske.png" class="img-fluid" style="width:50t; height:100pt;"></a>
+        <a class="navbar-brand" href="index.html">Barske</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -44,6 +30,52 @@ if ($conn->connect_error) {
         </div>
     </nav>
     <h1 style="text-align:center;">Final Project</h1>
+    <?php
+$servername = "localhost";
+$username = "emilypri_skeco";
+$password = "Projectdbskeco";
+$dbname = "emilypri_skeco";
+ 
+// Create connection 
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$sql = "SELECT * from Bar";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
+
+    <table class="table table-danger">
+  <thead>
+    <tr>
+      <th>Bar ID</th>
+      <th>Bar Name</th>
+      <th>State</th>
+      <th>City</th>
+    </tr>
+  </thead>
+  <tbody>
+  <tr>
+    <td><?=$row["BarID"]?></td>
+    <td><?=$row["Bar_Name"]?></td>
+    <td><?=$row["State"]?></td>
+    <td><?=$row["City"]?></td>
+  </tr>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+  </tbody>
+    </table>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
   </body>
