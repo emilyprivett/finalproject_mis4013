d1523aa4b7d234af986b19acc7f9ab742530c427
emilyprivett <78056325+emilyprivett@users.noreply.github.com>
2022-11-30 20:45:14 -0600

    Update index.php

--- index.php
+++ index.php
@@ -29,27 +29,35 @@
             </ul>
         </div>
     </nav>
-    <h1 style="text-align:center;">Welcome to Barske!</h1>    
- <div class="container">       
-  <div class="card-group" style="width: 18rem; display:inline-block;">
+    <h1 style="text-align:center;">Welcome to Barske!</h1>
+  <div class="card-group">
+  <div class="card" style="width: 18rem;">
   <img class="card-img-top" src="beehive.png">
   <div class="card-body">
     <h5 class="card-title">The Beehive</h5>
     <p class="card-text">564 Moore Avenue, Dallas TX, 75207</p>
     <a href="beehive.php" class="btn btn-primary">The Beehive</a>
-      
+  </div>
+  </div>
+  <div class="card" style="width: 18rem;">
   <img class="card-img-top" src="orchard.png">
   <div class="card-body">
     <h5 class="card-title">The Orchard</h5>
     <p class="card-text">2621 Brooke Street, Houston TX, 77035</p>
     <a href="orchard.php" class="btn btn-primary">The Orchard</a>
-        
+  </div>
+  </div>    
+  <div class="card" style="width: 18rem;">
   <img class="card-img-top" src="tiki.png">
   <div class="card-body">
     <h5 class="card-title">The Tipsy Tiki</h5>
     <p class="card-text">934 Short Street, Austin TX, 78723</p>
     <a href="tiki.php" class="btn btn-primary">The Tipsy Tiki</a>
-</div>
+  </div>
+  </div>
+  </div>
+  
+    
     <?php
 $servername = "localhost";
 $username = "emilypri_skeco";
