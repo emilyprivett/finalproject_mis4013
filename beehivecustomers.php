b45c82128874364fd0c4a696bec7efd088930238
Kylie Merz <78387839+kmerz5@users.noreply.github.com>
2022-12-02 16:48:07 -0600

    Update beehivecustomers.php

--- beehivecustomers.php
+++ beehivecustomers.php
@@ -53,7 +53,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-$sql = "SELECT CustomerID, Customer_FirstName,Customer_LastName, Customer_Age FROM Drink d JOIN Order o ON d.DrinkID=o.DrinkID JOIN Customer c ON o.OrderID=c.OrderID WHERE d.BarID=1";
+$sql = "SELECT CustomerID, Customer_FirstName,Customer_LastName, Customer_Age FROM Order o JOIN Customer c ON o.OrderID=c.OrderID";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
