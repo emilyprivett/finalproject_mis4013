a01531bf480bf2260d4f8eb4f76955c8d50d509b
Kylie Merz <78387839+kmerz5@users.noreply.github.com>
2022-12-02 14:35:12 -0600

    Update beehive.php

--- beehive.php
+++ beehive.php
@@ -76,7 +76,8 @@ $conn->close();
       </tbody>
     </table>
     <br />
-    <button type="button" class="btn btn-secondary btn-lg btn-block" href = "beehivecustomers.php">The BeeHive's Customers</button>
+    
+    <a href="beehivecustomers.php" class="btn btn-secondary btn-lg btn-block"> The BeeHive's Customers<a/>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
   </body>
 </html>
