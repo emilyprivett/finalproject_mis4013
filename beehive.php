b9ea53f9d279c2fd24cc54a819fe3eb7d774cb0b
Kylie Merz <78387839+kmerz5@users.noreply.github.com>
2022-12-03 13:06:23 -0600

    Update beehive.php

--- beehive.php
+++ beehive.php
@@ -77,7 +77,7 @@ $conn->close();
     </table>
     <br />
     
-    <a href="beehivecustomers.php" class="btn btn-secondary btn-lg btn-block"> The BeeHive's Customers<a/>
+    <a href="customers.php" class="btn btn-secondary btn-lg btn-block"> Our Customers<a/>
     <a href="beehiveemployees.php" class="btn btn-secondary btn-lg btn-block"> The BeeHive's Employees<a/>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
   </body>
