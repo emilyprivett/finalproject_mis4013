def32612483004305751917f0144f845c7f612be
Kylie Merz <78387839+kmerz5@users.noreply.github.com>
2022-12-03 13:12:14 -0600

    Update tiki.php

--- tiki.php
+++ tiki.php
@@ -77,7 +77,7 @@ $conn->close();
     </table>
     <br />
     
-    <a href="tikicustomers.php" class="btn btn-secondary btn-lg btn-block"> The Tipsy Tiki's Customers<a/>
+    <a href="customers.php" class="btn btn-secondary btn-lg btn-block"> Barske Customers<a/>
     <a href="tikiemployees.php" class="btn btn-secondary btn-lg btn-block"> The Tipsy Tiki's Employees<a/>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
   </body>
