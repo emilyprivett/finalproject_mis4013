11357523b9c95d18a65057de419bcdfbe13c994c
Kylie Merz <78387839+kmerz5@users.noreply.github.com>
2022-12-03 13:23:19 -0600

    Update customers-add-save.php

--- customers-add-save.php
+++ customers-add-save.php
@@ -1,1 +1,39 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Add Customer</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+    
+<?php  
+$servername = "localhost";
+$username = "kyliemer_Homework3";
+$password = "ChunkyMonkey01!";
+$dbname = "kyliemer_Homework3";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$cName = $_POST['cName'];
+
+$sql = "insert into Customer (Customer_FirstName, Customer_LastName, Customer_Age) values (?,?,?)";
+//echo $sql;
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ssi", $cName,$_POST['lName'],$_POST['cAge']);
+    $stmt->execute();
+?>
+    
+    <h1>Add Customer</h1>
+<div class="alert alert-success" role="alert">
+  New customer added.
+</div>
+    <a href="customers.php" class="btn btn-primary">Go back</a>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
