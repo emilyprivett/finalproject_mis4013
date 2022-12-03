b38bb6eb7a0e93fef134fd85d64815d7553c2c47
Kylie Merz <78387839+kmerz5@users.noreply.github.com>
2022-12-03 13:29:09 -0600

    Update customers-edit.php

--- customers-edit.php
+++ customers-edit.php
@@ -1,1 +1,65 @@
 
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Edit Customer</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>Edit Customer</h1>
+<?php
+$servername = "localhost";
+$username = "emilypri_skeco";
+$password = "Projectdbskeco";
+$dbname = "emilypri_skeco";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM Customer WHERE Customer_ID=?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $_POST['cid']);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
+
+<form method="post" action="customers-edit-save.php">
+  <div class="mb-3">
+    <label for="Customer_FirstName" class="form-label">Name</label>
+    <input type="text" class="form-control" id="Customer_FirstName" aria-describedby="nameHelp" name="cName">
+    <div id="nameHelp" class="form-text">Enter the customer's first name.</div>
+  </div>
+  <div class="mb-3">
+    <label for="Customer_LastName" class="form-label">Name</label>
+    <input type="text" class="form-control" id="Customer_LastName" aria-describedby="nameHelp" name="lName">
+    <div id="nameHelp" class="form-text">Enter the customer's last name.</div>
+  </div>
+  <div class="mb-3">
+    <label for="Customer_Age" class="form-label">Age</label>
+    <input type="text" class="form-control" id="Customer_Age" aria-describedby="nameHelp" name="cAge">
+    <div id="nameHelp" class="form-text">Enter the customer's age.</div>
+  </div>
+  <input type="hidden" name="cid" value="<?=$row['Customer_ID']?>">
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
