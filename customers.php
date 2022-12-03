3491481d2a6bb042cd7bbd3bb4e155c20d2bb4e5
Kylie Merz <78387839+kmerz5@users.noreply.github.com>
2022-12-03 13:41:27 -0600

    Update customers.php

--- customers.php
+++ customers.php
@@ -29,7 +29,7 @@
             </ul>
         </div>
     </nav>
-    <h1 style="text-align:center;">The Beehive</h1>
+    <h1 style="text-align:center;">Barske Customer's</h1>
     <table class="table table-striped">
       <thead>
         <tr>
@@ -53,9 +53,34 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  switch ($_POST['saveType']) {
+    case 'Add':
+      $sqlAdd = "INSERT INTO Customer (Customer_FirstName, Customer_LastName, Customer_Age) values (?,?,?)";
+      $stmtAdd = $conn->prepare($sqlAdd);
+      $stmtAdd->bind_param("ssi", $_POST['cName'],$_POST['lName'],$_POST['cAge']);
+      $stmtAdd->execute();
+      echo '<div class="alert alert-success" role="alert">New customer added.</div>';
+      break;
+    case 'Edit':
+      $sqlEdit = "UPDATE Customer SET Customer_FirstName=?, Customer_LastName=?, Customer_Age=? WHERE Customer_ID=?";
+      $stmtEdit = $conn->prepare($sqlEdit);
+      $stmtEdit->bind_param("ssii", $_POST['cName'],$_POST['lName'],$_POST['CAge'],$_POST['cid']);
+      $stmtEdit->execute();
+      echo '<div class="alert alert-success" role="alert">Customer edited.</div>';
+      break;
+    case 'Delete':
+      $sqlDelete = "DELETE FROM Customer WHERE Customer_ID=?";
+      $stmtDelete = $conn->prepare($sqlDelete);
+      $stmtDelete->bind_param("i", $_POST['cid']);
+      $stmtDelete->execute();
+      echo '<div class="alert alert-success" role="alert">Customer deleted.</div>';
+      break;
+  }
+}        
         
-$sql = "SELECT * FROM Customer";
-        
+$sql = "SELECT * FROM Customer"; 
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
@@ -117,7 +142,44 @@ $conn->close();
       </tbody>
     </table>
     <br />
-    <a href="customers-add.php" class="btn btn-primary"> Add New<a/>
+    <!-- Button trigger modal -->
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addCustomer">
+        Add New
+      </button>
+
+      <!-- Modal -->
+      <div class="modal fade" id="addCustomer" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="addCustomerLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="addCustomerLabel">Add Customer</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <form method="post" action="">
+                <div class="mb-3">
+                  <label for="Customer_FirstName" class="form-label">First Name</label>
+                  <input type="text" class="form-control" id="Customer_FirstName" aria-describedby="nameHelp" name="cName">
+                  <div id="nameHelp" class="form-text">Enter the customer's first name.</div>
+                </div>
+                <div class="mb-3">
+                  <label for="Customer_LastName" class="form-label">Last Name</label>
+                  <input type="text" class="form-control" id="Customer_LastName" aria-describedby="nameHelp" name="lName">
+                  <div id="nameHelp" class="form-text">Enter the customer's last name.</div>
+                </div>
+                <div class="mb-3">
+                  <label for="Customer_Age" class="form-label">Customer Age</label>
+                  <input type="text" class="form-control" id="Customer_Age" aria-describedby="nameHelp" name="cAge">
+                  <div id="nameHelp" class="form-text">Enter the customer's Age.</div>
+                </div>
+                <input type="hidden" name="saveType" value="Add">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
   </body>
 </html>
