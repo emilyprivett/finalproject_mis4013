7c11640225300ac70c0726839bb5890e0df6a89f
emilyprivett <78056325+emilyprivett@users.noreply.github.com>
2022-12-05 19:07:48 -0600

    Update aboutus.php

--- aboutus.php
+++ aboutus.php
@@ -6,8 +6,36 @@
     <title>Barske</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
   </head>
-  <h1 style="text-alighn:center;">About Barske Founders</h1>
+  <body style="background-color: #F8EDDB">
+     <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #A6B6E9">
+        <a class="navbar-brand" href="index.php"><img src="barske.png" class="img-fluid" style="height:35pt;"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="beehive.php">The Beehive</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="orchard.php">The Orchard</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="tiki.php">The Tipsy Tiki</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="review.php">Drink Reviews</a>
+                </li>
+               <li class="nav-item">
+                    <a class="nav-link" href="aboutus.php">About Barske Founders</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <h1 style="text-align:center;">About the Barske Founders</h1>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
   </body>
 </html>
+
 
