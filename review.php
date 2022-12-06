37bdc697319665bb12b11a09869984883513b7af
emilyprivett <78056325+emilyprivett@users.noreply.github.com>
2022-12-05 19:25:27 -0600

    Update review.php

--- review.php
+++ review.php
@@ -56,7 +56,7 @@ if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
 ?>
-<div id="myGoogleChart" style="width:100%; max-width:700px; height:600px;"></div>
+<div id="myGoogleChart" style="width:100%; max-width:700px; height:600px; text-align:center;"></div>
 
     <script>
         google.charts.load('current', { 'packages': ['corechart'] });
