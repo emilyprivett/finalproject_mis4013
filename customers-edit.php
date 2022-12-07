f00f523c3ea63e44e0b3cc72f1975bbcc1a4adaf
emilyprivett <78056325+emilyprivett@users.noreply.github.com>
2022-12-06 19:35:08 -0600

    Update customers-edit.php

--- customers-edit.php
+++ customers-edit.php
@@ -4,11 +4,11 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Edit Customer</title>
+    <title style="font-family:Rockwell; font-weight:bold;">Edit Customer</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Edit Customer</h1>
+    <h1 style="font-family:Rockwell; font-weight:bold;">Edit Customer</h1>
 <?php
 $servername = "localhost";
 $username = "emilypri_skeco";
@@ -35,22 +35,22 @@ if ($result->num_rows > 0) {
 
 <form method="post" action="customers-edit-save.php">
   <div class="mb-3">
-    <label for="Customer_FirstName" class="form-label">Name</label>
+    <label for="Customer_FirstName" class="form-label" style="font-family:Rockwell;">Name</label>
     <input type="text" class="form-control" id="Customer_FirstName" aria-describedby="nameHelp" name="cName">
-    <div id="nameHelp" class="form-text">Enter the customer's first name.</div>
+    <div id="nameHelp" class="form-text" style="font-family:Rockwell;">Enter the customer's first name.</div>
   </div>
   <div class="mb-3">
-    <label for="Customer_LastName" class="form-label">Name</label>
+    <label for="Customer_LastName" class="form-label" style="font-family:Rockwell;">Name</label>
     <input type="text" class="form-control" id="Customer_LastName" aria-describedby="nameHelp" name="lName">
-    <div id="nameHelp" class="form-text">Enter the customer's last name.</div>
+    <div id="nameHelp" class="form-text" style="font-family:Rockwell;">Enter the customer's last name.</div>
   </div>
   <div class="mb-3">
-    <label for="Customer_Age" class="form-label">Age</label>
+    <label for="Customer_Age" class="form-label" style="font-family:Rockwell;">Age</label>
     <input type="int" class="form-control" id="Customer_Age" aria-describedby="nameHelp" name="cAge">
-    <div id="nameHelp" class="form-text">Enter the customer's age.</div>
+    <div id="nameHelp" class="form-text" style="font-family:Rockwell;">Enter the customer's age.</div>
   </div>
   <input type="hidden" name="cid" value="<?=$row['CustomerID']?>">
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" class="btn btn-primary" style="font-family:Rockwell;">Submit</button>
 </form>
 <?php
   }
