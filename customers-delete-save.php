e27a8c5a1500ac534b314bda2d73b98ff147f0a2
emilyprivett <78056325+emilyprivett@users.noreply.github.com>
2022-12-06 19:33:09 -0600

    Update customers-delete-save.php

--- customers-delete-save.php
+++ customers-delete-save.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Delete Customer</title>
+    <title style="font-family:Rockwell; font-weight:bold;">Delete Customer</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
@@ -28,11 +28,11 @@ $sql = "delete from Customer where CustomerID=?";
     $stmt->execute();
 ?>
     
-    <h1>Delete Customer</h1>
+    <h1 style="font-family:Rockwell; font-weight:bold;">Delete Customer</h1>
 <div class="alert alert-success" role="alert">
   Customer deleted.
 </div>
-    <a href="customers.php" class="btn btn-primary">Go back</a>
+    <a href="customers.php" class="btn btn-primary" style="font-family:Rockwell;">Go back</a>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
