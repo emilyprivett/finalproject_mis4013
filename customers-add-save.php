bc29275e7e9b5a60fc05c71d8617ff289cc812b5
emilyprivett <78056325+emilyprivett@users.noreply.github.com>
2022-12-06 19:29:39 -0600

    Update customers-add-save.php

--- customers-add-save.php
+++ customers-add-save.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Add Customer</title>
+    <title style="font-family:Rockwell; font-weight: bold;">Add Customer</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
@@ -29,11 +29,11 @@ $sql = "insert into Customer (Customer_FirstName, Customer_LastName, Customer_Ag
     $stmt->execute();
 ?>
     
-    <h1>Add Customer</h1>
-<div class="alert alert-success" role="alert">
+    <h1 style="font-family:Rockwell; font-weight:bold;">Add Customer</h1>
+<div class="alert alert-success" role="alert" style="font-family:Rockwell;">
   New customer added.
 </div>
-    <a href="customers.php" class="btn btn-primary">Go back</a>
+    <a href="customers.php" class="btn btn-primary" style="font-family:Rockwell;">Go back</a>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
