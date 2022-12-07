b2c9bca4741c34107b8d59edf1312420e2e711f9
emilyprivett <78056325+emilyprivett@users.noreply.github.com>
2022-12-06 19:16:09 -0600

    Update aboutus.php

--- aboutus.php
+++ aboutus.php
@@ -8,7 +8,7 @@
    <link rel="stylesheet" href="aboutus.css">
   </head>
   <body style="background-color: #F8EDDB">
-     <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #A6B6E9">
+     <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #A6B6E9; font-family:Rockwell !important;">
         <a class="navbar-brand" href="index.php"><img src="barske.png" class="img-fluid" style="height:35pt;"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
